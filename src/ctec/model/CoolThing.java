c6f63fab08378c9e8108660a19362d3701f676ed
Zach <zachmoy17@gmail.com>
2016-04-04 17:09:24 -0600

    added javadoc

--- src/ctec/model/CoolThing.java
+++ src/ctec/model/CoolThing.java
@@ -1,11 +1,16 @@
 package ctec.model;
 
+/**
+ * Interface with two methods with a parameter and two methods with a return value.
+ * @author Zach, Greyson, Billy, Alejandro
+ * @version 1.0
+ */
+
 public interface CoolThing
 {
 	public double coolnessLevel();
 	public void coolAction(int numberOfTimes);
 	public int cost();
 	public void EthanBradberry(int numberOfTimes);
-	
 	
 }
